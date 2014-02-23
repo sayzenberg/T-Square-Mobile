0fb82c082fa34dd653b77d6c6a4c19c0c34d00a1
Ayzenberg, Stanley Roger <sayzenberg3@gatech.edu>
2014-02-23 16:14:03 -0500

    Made progress on basic layout

--- Android/T-SquareMobile/src/com/teammeh/t_squaremobile/HomeScreenActivity.java
+++ Android/T-SquareMobile/src/com/teammeh/t_squaremobile/HomeScreenActivity.java
@@ -2,27 +2,21 @@ package com.teammeh.t_squaremobile;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
 import android.view.Menu;
-import android.view.View;
 
-public class LoginActivity extends Activity {
+public class HomeScreenActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_login);
+		setContentView(R.layout.activity_home_screen);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.login, menu);
+		getMenuInflater().inflate(R.menu.home_screen, menu);
 		return true;
 	}
 
-	public void onPress(View view) {
-		Intent intent = new Intent(this, HomeScreenActivity.class);
-		startActivity(intent);
-	}
 }
